c1c5b80973c8ac6ecf119bc540dea1e6ac78e093
toantran292 <toan@boompay.app>
2024-11-19 23:35:16 +0700

    update util and constants.ts

--- source/lib/utils.ts
+++ source/lib/utils.ts
@@ -1,242 +1,85 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
-import {
-  GRAPH_ALGO,
-  GRAPH_ALGO_NAME,
-  GRAPH_TOPIC,
-  GRAPH_TOPIC_ALGOS,
-  MINIMUM_SPANNING_TREE_ALGO,
-  SHORTEST_PATH_ALGO,
-} from "@/app/constants";
-import dijkstra from "@/app/algorthms/shortest_paths/dijkstra";
-import { MarkerType } from "@xyflow/react";
-import floyd from "@/app/algorthms/shortest_paths/floyd";
-import prim from "@/app/algorthms/minimun_spanning_tree/prim";
+import { ALGORITHMS, TOPICS } from "@/app/constants";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export function isDirectedMatrix(matrix: number[][]): boolean {
-  for (let i = 0; i < matrix.length; i++) {
-    for (let j = 0; j < matrix[i].length; j++) {
-      if (matrix[i][j] !== matrix[j][i]) {
-        return true;
+export type Matrix = number[][];
+
+export const checkAlgorithms = (matrix: Matrix): Record<string, string[]> => {
+  const n = matrix.length;
+
+  let hasNegativeWeights = false;
+  let hasNegativeWeightCycles = false;
+  let isDirected = false;
+  let hasInfinity = false;
+
+  // Check matrix properties
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      if (matrix[i][j] < 0) hasNegativeWeights = true;
+      if (matrix[i][j] === Infinity) hasInfinity = true;
+      if (matrix[i][j] !== matrix[j][i]) isDirected = true;
+    }
+  }
+
+  // Check for negative weight cycles using Floyd-Warshall
+  const dist = Array.from({ length: n }, (_, i) =>
+    Array.from({ length: n }, (_, j) =>
+      i === j ? 0 : matrix[i][j] || Infinity,
+    ),
+  );
+
+  for (let k = 0; k < n; k++) {
+    for (let i = 0; i < n; i++) {
+      for (let j = 0; j < n; j++) {
+        if (dist[i][j] > dist[i][k] + dist[k][j]) {
+          dist[i][j] = dist[i][k] + dist[k][j];
+        }
       }
     }
   }
-  return false;
-}
 
-export class Matrix {
-  matrix: number[][];
-
-  constructor(matrix: number[][]) {
-    this.matrix = matrix;
-  }
-
-  n() {
-    return this.matrix.length;
-  }
-
-  get_nodes(algo: any, source: any = -1) {
-    return this.matrix.map((_, i) => {
-      const res = {
-        id: i.toString(),
-        data: { label: `${i}` },
-        position: { x: 0, y: 0 },
-        style: {
-          borderRadius: "100%",
-          backgroundColor: "#fff",
-          width: 30,
-          height: 30,
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-        },
-      };
-
-      if (
-        !GRAPH_TOPIC_ALGOS[GRAPH_TOPIC.MINIMUM_SPANNING_TREE].includes(algo) &&
-        i === source
-      ) {
-        res.style = {
-          ...res.style,
-          backgroundColor: "red",
-          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-          // @ts-expect-error
-          color: "white",
-        };
-      }
-
-      return res;
-    });
-  }
-
-  get(i: number, j: number) {
-    return this.matrix[i][j];
-  }
-
-  to_path_floyd(
-    nodes: any,
-    isDirected: boolean,
-    source,
-    onlyResult: boolean = false,
-  ) {
-    return { nodes, edges: [] };
-  }
-
-  to_path_dijkstra(
-    nodes: any,
-    isDirected: boolean,
-    source: number = 0,
-    onlyResult: boolean = false,
-  ) {
-    const { trace } = dijkstra(this.matrix, source);
-
-    const temp = trace.reduce((acc, source, target) => {
-      if (source !== -1) {
-        acc.push(`e${source}${target}`);
-        acc.push(`e${target}${source}`);
-      }
-      return acc;
-    }, []) as string[];
-
-    const edges = trace.reduce((acc, source, target) => {
-      if (source !== -1) {
-        acc.push({
-          id: `e${source}${target}`,
-          source: source.toString(),
-          target: target.toString(),
-          data: {
-            label: this.get(source, target).toString(),
-          },
-          type: "floating",
-          style: {
-            strokeWidth: 2,
-            stroke: "green",
-          },
-          markerEnd: {
-            type: MarkerType.ArrowClosed,
-            width: 10,
-            height: 10,
-            color: "green",
-          },
-        });
-      }
-      return acc;
-    }, []);
-
-    if (!onlyResult)
-      edges.push(
-        ...this.to_path_default(nodes, isDirected).edges.filter(
-          (edge) => !temp.includes(edge.id),
-        ),
-      );
-
-    console.log({ nodes, edges });
-    return { nodes, edges };
-  }
-
-  to_path_prim(nodes: any, onlyResult: boolean = false) {
-    const { parent, cost } = prim(this.matrix);
-
-    const temp = parent.reduce((acc, source, target) => {
-      if (source !== -1) {
-        acc.push(`e${source}${target}`);
-        acc.push(`e${target}${source}`);
-      }
-      return acc;
-    }, []) as string[];
-
-    const edges = parent.reduce((acc, source, target) => {
-      if (source !== -1) {
-        acc.push({
-          id: `e${source}${target}`,
-          source: source.toString(),
-          target: target.toString(),
-          data: {
-            label: this.get(source, target).toString(),
-          },
-          type: "floating",
-          style: {
-            strokeWidth: 2,
-            stroke: "green",
-          },
-        });
-      }
-      return acc;
-    }, []);
-
-    if (!onlyResult)
-      edges.push(
-        ...this.to_path_default(nodes, false).edges.filter(
-          (edge) => !temp.includes(edge.id),
-        ),
-      );
-
-    return { nodes, edges };
-  }
-
-  to_path_default(nodes: any, isDirected: boolean) {
-    const edges = this.matrix.flatMap((line, i) =>
-      line
-        .map((item, j) => {
-          const res = {
-            id: `e${i}${j}`,
-            source: i.toString(),
-            target: j.toString(),
-            weight: this.get(i, j).toString(),
-            data: {
-              label: this.get(i, j).toString(),
-            },
-            type: "floating",
-            style: {
-              strokeWidth: 2,
-              stroke: "black",
-            },
-          };
-          if (isDirected) {
-            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-            // @ts-expect-error
-            res.markerEnd = {
-              type: MarkerType.ArrowClosed,
-              width: 10,
-              height: 10,
-              color: "black",
-            };
-          }
-          return res;
-        })
-        .filter((edge) => {
-          return (
-            edge.source !== edge.target &&
-            edge.weight !== "0" &&
-            (isDirected || edge.source < edge.target)
-          );
-        }),
-    );
-
-    return { nodes, edges };
-  }
-
-  to_path(
-    algo: GRAPH_ALGO | null,
-    source?: number,
-    onlyResult: boolean = false,
-  ) {
-    const isDirected = isDirectedMatrix(this.matrix);
-    const nodes = this.get_nodes(algo, source);
-
-    switch (algo) {
-      case SHORTEST_PATH_ALGO.DIJKSTRA:
-        return this.to_path_dijkstra(nodes, isDirected, source, onlyResult);
-      case SHORTEST_PATH_ALGO.FLOYD:
-        return this.to_path_floyd(nodes, isDirected, source, onlyResult);
-      case MINIMUM_SPANNING_TREE_ALGO.PRIM:
-        return this.to_path_prim(nodes, onlyResult);
-      default:
-        return this.to_path_default(nodes, isDirected);
+  for (let i = 0; i < n; i++) {
+    if (dist[i][i] < 0) {
+      hasNegativeWeightCycles = true;
+      break;
     }
   }
-}
+
+  const applicableAlgorithms: Record<string, string[]> = {
+    [TOPICS.SHORTEST_PATH]: [],
+    [TOPICS.TSP]: [],
+    [TOPICS.MINIMUM_SPANNING_TREE]: [],
+    [TOPICS.GRAPH_COLORING]: [],
+  };
+
+  // Shortest Path
+  if (!hasNegativeWeights) {
+    applicableAlgorithms[TOPICS.SHORTEST_PATH].push(ALGORITHMS.DIJKSTRA);
+  }
+  if (!hasNegativeWeightCycles) {
+    applicableAlgorithms[TOPICS.SHORTEST_PATH].push(ALGORITHMS.FLOYD);
+  }
+
+  // TSP
+  if (!hasInfinity) {
+    applicableAlgorithms[TOPICS.TSP].push(ALGORITHMS.BRUTE_FORCE);
+    applicableAlgorithms[TOPICS.TSP].push(ALGORITHMS.BRANCH_AND_BOUND);
+  }
+
+  // Minimum Spanning Tree
+  if (!isDirected) {
+    applicableAlgorithms[TOPICS.MINIMUM_SPANNING_TREE].push(ALGORITHMS.KRUSKAL);
+    if (!hasNegativeWeights) {
+      applicableAlgorithms[TOPICS.MINIMUM_SPANNING_TREE].push(ALGORITHMS.PRIM);
+    }
+  }
+
+  // Graph Coloring
+  applicableAlgorithms[TOPICS.GRAPH_COLORING].push(ALGORITHMS.BACKTRACKING);
+
+  return applicableAlgorithms;
+};
